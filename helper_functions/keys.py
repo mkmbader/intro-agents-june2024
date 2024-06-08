14287b62520d29572fd1688885ef355edaa5ed00
Maria <maria@Marias-MacBook-Pro.local>
2024-06-08 22:11:17 +0200

    advanced agent + tools in separate file

---
+++ helper_functions/keys.py
@@ -0,0 +1,8 @@
+# insert here the keys from: <bin>
+
+from openai import OpenAI
+
+WEATHER_KEY = 'ZHK4HRAZ3MTNV5FDNQKFYUGL7'
+
+OPENAI_KEY = 'sk-proj-5BJfhNFfrPUy3EZE90VKT3BlbkFJcbj9y7W1ecH11puQ6ixw'
+client = OpenAI(api_key = OPENAI_KEY)
