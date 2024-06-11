7d89e3f4c4435cce916463be197dfaf7c5476df8
Maria <maria@Marias-MacBook-Pro.local>
2024-06-11 11:13:25 +0200

    remove refernce to old bin

--- helper_functions/keys.py
+++ helper_functions/keys.py
@@ -1,8 +1,8 @@
-# insert here the keys from: https://privatebin.molops.io/?458e3243227ac950#5ggrDtv3ZM3Ns6mPLKdhN9jTQYkyBfRzTuaenJDHmSy8
+# insert here the keys 
 
 from openai import OpenAI
 
 WEATHER_KEY = ''
 
-OPENAI_KEY = 'sk-proj-'
+OPENAI_KEY = ''
 client = OpenAI(api_key = OPENAI_KEY)
